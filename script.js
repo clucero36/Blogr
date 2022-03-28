ebb16f84744049a66a940e3053d8e194b04020ca
Luis Lucero <clucero3696@gmail.com>
2022-03-28 13:32:31 -0700

    Added Comments

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
+/* scripts used to add functionality to hamburger and dropdown lists */
+
+
 const intro__dropdown = document.querySelector('.intro__dropdown');
 const intro__burger = document.querySelector('.intro__burger');
 
 const intro__dropdownSelectionOne = document.querySelector('.intro__dropdown--selection-one');
 const intro__dropdownSelectionTwo = document.querySelector('.intro__dropdown--selection-two');
 const intro__dropdownSelectionThree = document.querySelector('.intro__dropdown--selection-three');
+
 const intro__dropdownListOne = document.querySelector('.intro__dropdown--list-one');
 const intro__dropdownListTwo = document.querySelector('.intro__dropdown--list-two');
 const intro__dropdownListThree = document.querySelector('.intro__dropdown--list-three');
 
+// Toggles dropdown list
 intro__burger.addEventListener('click', () => {
     intro__dropdown.classList.toggle('show');
 })
 
+// Toggles dropdown list option1
 intro__dropdownSelectionOne.addEventListener('click', () => {
     intro__dropdownListOne.classList.toggle('show');
 })
 
+// Toggles dropdown list option2
 intro__dropdownSelectionTwo.addEventListener('click', () => {
     intro__dropdownListTwo.classList.toggle('show');
 })
 
+// Toggles dropdown list option3
 intro__dropdownSelectionThree.addEventListener('click', () => {
     intro__dropdownListThree.classList.toggle('show');
 })
